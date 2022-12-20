bd189fd9933220cf67c0cbd6192b87f957f0f451
mhdhaid <mhd4.hz@gmail.com>
2022-12-21 01:07:09 +0300

    base

--- app/src/main/java/svu/org/homework/ChemistryQ3Activity.java
+++ app/src/main/java/svu/org/homework/ChemistryQ3Activity.java
@@ -2,8 +2,10 @@ package svu.org.homework;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class ChemistryQ3Activity extends AppCompatActivity {
     MediaPlayer mp;
@@ -12,5 +14,10 @@ public class ChemistryQ3Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chemistry_q3);
         mp = MediaPlayer.create(this, R.raw.soho);
+    }
+    public void btnClickNext(View V){
+        mp.start();
+        Intent i = new Intent(this,ChemistryQ2Activity.class);
+        startActivity(i);
     }
 }
