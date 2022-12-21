d56fbf0164f71672c5a14d93c4c3159e6b2d96a9
Mohammad Haidar <mhd4.hz@gmail.com>
2022-12-21 14:54:46 +0300

    base

--- app/src/main/java/svu/org/homework/MathQ1Activity.java
+++ app/src/main/java/svu/org/homework/MathQ1Activity.java
@@ -2,8 +2,10 @@ package svu.org.homework;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class MathQ1Activity extends AppCompatActivity {
     MediaPlayer mp;
@@ -12,5 +14,10 @@ public class MathQ1Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_math_q1);
         mp = MediaPlayer.create(this, R.raw.soho);
+    }
+    public void btnClickNext(View V){
+        mp.start();
+        Intent i = new Intent(this,MathQ2Activity.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/svu/org/homework/MathQ2Activity.java
+++ app/src/main/java/svu/org/homework/MathQ2Activity.java
@@ -2,8 +2,10 @@ package svu.org.homework;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class MathQ2Activity extends AppCompatActivity {
     MediaPlayer mp;
@@ -12,5 +14,15 @@ public class MathQ2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_math_q2);
         mp = MediaPlayer.create(this, R.raw.soho);
+    }
+    public void btnClickNext(View V){
+        mp.start();
+        Intent i = new Intent(this,MathQ3Activity.class);
+        startActivity(i);
+    }
+    public void btnClickBack(View V){
+        mp.start();
+        Intent i = new Intent(this,MathQ1Activity.class);
+        startActivity(i);
     }
 }
