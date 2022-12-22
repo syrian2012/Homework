448d52eea4e53bc7c96425b5f9a921ef8d67d30d
mhdhaid <mhd4.hz@gmail.com>
2022-12-22 15:07:15 +0300

    base

--- app/src/main/java/svu/org/homework/ChemistryQ1Activity.java
+++ app/src/main/java/svu/org/homework/ChemistryQ1Activity.java
@@ -6,9 +6,16 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class ChemistryQ1Activity extends AppCompatActivity {
     MediaPlayer mp;
+    RadioButton rb_Q1WA1Chemistry = findViewById(R.id.rb_Q1WA1Chemistry);
+    RadioButton rb_Q1WA2Chemistry = findViewById(R.id.rb_Q1WA2Chemistry);
+    RadioButton rb_Q1WA3Chemistry = findViewById(R.id.rb_Q1WA3Chemistry);
+    RadioButton rb_Q1RAChemistry = findViewById(R.id.rb_Q1RAChemistry);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +25,11 @@ public class ChemistryQ1Activity extends AppCompatActivity {
 
     public void btnClickNext(View V){
         mp.start();
+        if (rb_Q1WA1Chemistry.isChecked()==false&&rb_Q1WA2Chemistry.isChecked()==false&&rb_Q1WA3Chemistry.isChecked()==false&&rb_Q1RAChemistry.isChecked()==false){
+            Toast.makeText(getApplicationContext(),"Please choose an answer to go to another question",Toast.LENGTH_LONG).show();
+        }
+        else {
         Intent i = new Intent(this,ChemistryQ2Activity.class);
         startActivity(i);
-    }
+    }}
 }
