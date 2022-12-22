d9b599279ed4fdbcfebd73ea27613a8537569893
mhdhaid <mhd4.hz@gmail.com>
2022-12-22 15:14:51 +0300

    base

--- app/src/main/java/svu/org/homework/ChemistryQ1Activity.java
+++ app/src/main/java/svu/org/homework/ChemistryQ1Activity.java
@@ -11,25 +11,29 @@ import android.widget.Toast;
 
 public class ChemistryQ1Activity extends AppCompatActivity {
     MediaPlayer mp;
-    RadioButton rb_Q1WA1Chemistry = findViewById(R.id.rb_Q1WA1Chemistry);
-    RadioButton rb_Q1WA2Chemistry = findViewById(R.id.rb_Q1WA2Chemistry);
-    RadioButton rb_Q1WA3Chemistry = findViewById(R.id.rb_Q1WA3Chemistry);
-    RadioButton rb_Q1RAChemistry = findViewById(R.id.rb_Q1RAChemistry);
+    RadioButton rb_Q1WA1Chemistry;
+    RadioButton rb_Q1WA2Chemistry;
+    RadioButton rb_Q1WA3Chemistry;
+    RadioButton rb_Q1RAChemistry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chemistry_q1);
         mp = MediaPlayer.create(this, R.raw.soho);
+        rb_Q1WA1Chemistry = findViewById(R.id.rb_Q1WA1Chemistry);
+        rb_Q1WA2Chemistry = findViewById(R.id.rb_Q1WA2Chemistry);
+        rb_Q1WA3Chemistry = findViewById(R.id.rb_Q1WA3Chemistry);
+        rb_Q1RAChemistry = findViewById(R.id.rb_Q1RAChemistry);
     }
 
     public void btnClickNext(View V){
         mp.start();
-        if (rb_Q1WA1Chemistry.isChecked()==false&&rb_Q1WA2Chemistry.isChecked()==false&&rb_Q1WA3Chemistry.isChecked()==false&&rb_Q1RAChemistry.isChecked()==false){
+        if (!rb_Q1WA1Chemistry.isChecked()&&!rb_Q1WA2Chemistry.isChecked()&&!rb_Q1WA3Chemistry.isChecked()&&!rb_Q1RAChemistry.isChecked()){
             Toast.makeText(getApplicationContext(),"Please choose an answer to go to another question",Toast.LENGTH_LONG).show();
         }
         else {
         Intent i = new Intent(this,ChemistryQ2Activity.class);
-        startActivity(i);
-    }}
+        startActivity(i);}
+    }
 }
