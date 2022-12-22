318bd8de7a00ef658e7b9d2ecfab4b463e87cb36
mhdhaid <mhd4.hz@gmail.com>
2022-12-22 20:03:06 +0300

    base

--- app/src/main/java/svu/org/homework/ChemistryQ1Activity.java
+++ app/src/main/java/svu/org/homework/ChemistryQ1Activity.java
@@ -19,23 +19,26 @@ public class ChemistryQ1Activity extends AppCompatActivity {
     RadioButton rb_Q1RAChemistry;
     boolean Q1Answer;
     String username;
-
+    boolean sound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chemistry_q1);
-        mp = MediaPlayer.create(this, R.raw.soho);
+        if (sound) {
+            mp = MediaPlayer.create(this, R.raw.soho);
+        }
         rb_Q1WA1Chemistry = findViewById(R.id.rb_Q1WA1Chemistry);
         rb_Q1WA2Chemistry = findViewById(R.id.rb_Q1WA2Chemistry);
         rb_Q1WA3Chemistry = findViewById(R.id.rb_Q1WA3Chemistry);
         rb_Q1RAChemistry = findViewById(R.id.rb_Q1RAChemistry);
         username = getIntent().getStringExtra("username");
-
+        sound=false;
     }
 
     public void btnClickNext(View V){
-        mp.start();
+        if(sound){
+        mp.start();}
         if (!rb_Q1WA1Chemistry.isChecked()&&!rb_Q1WA2Chemistry.isChecked()&&!rb_Q1WA3Chemistry.isChecked()&&!rb_Q1RAChemistry.isChecked()){
             Toast.makeText(getApplicationContext(),"Please choose an answer to go to another question",Toast.LENGTH_LONG).show();
         }
