614476f4ce05641061b2f9bbf1e65dd2f857b245
mhdhaid <mhd4.hz@gmail.com>
2022-12-23 19:49:41 +0300

    base

--- app/src/main/java/svu/org/homework/ChemistryQ1Activity.java
+++ app/src/main/java/svu/org/homework/ChemistryQ1Activity.java
@@ -3,6 +3,7 @@ package svu.org.homework;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Context;
 import android.content.Intent;
@@ -11,8 +12,11 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ChemistryQ1Activity extends AppCompatActivity {
@@ -25,6 +29,10 @@ public class ChemistryQ1Activity extends AppCompatActivity {
     String username;
     boolean sound;
     SharedPreferences sp;
+    TextView tv_1,tv_2,tv_3;
+    Button bt_next;
+    ConstraintLayout rl;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +46,46 @@ public class ChemistryQ1Activity extends AppCompatActivity {
         rb_Q1WA3Chemistry = findViewById(R.id.rb_Q1WA3Chemistry);
         rb_Q1RAChemistry = findViewById(R.id.rb_Q1RAChemistry);
         username = getIntent().getStringExtra("username");
+        tv_1=findViewById(R.id.textView);
+        tv_2=findViewById(R.id.textView5);
+        tv_3=findViewById(R.id.textView6);
+        bt_next = findViewById(R.id.bt_nextQ1Chemistry);
+        rl =  findViewById(R.id.my_layout);
+        switch(sp.getString("Style","Style1")){
+            case "Style1":
+                rb_Q1WA1Chemistry.setTextColor(getColor(R.color.gold));
+                rb_Q1WA2Chemistry.setTextColor(getColor(R.color.gold));
+                rb_Q1WA3Chemistry.setTextColor(getColor(R.color.gold));
+                rb_Q1RAChemistry.setTextColor(getColor(R.color.gold));
+                tv_1.setTextColor(getColor(R.color.gold));
+                tv_2.setTextColor(getColor(R.color.gold));
+                tv_3.setTextColor(getColor(R.color.gold));
+                bt_next.setBackgroundColor(getColor(R.color.gold));
+                rl.setBackgroundColor(getColor(R.color.white));
+                break;
+            case "Style2":
+                rb_Q1WA1Chemistry.setTextColor(getColor(R.color.cyan));
+                rb_Q1WA2Chemistry.setTextColor(getColor(R.color.cyan));
+                rb_Q1WA3Chemistry.setTextColor(getColor(R.color.cyan));
+                rb_Q1RAChemistry.setTextColor(getColor(R.color.cyan));
+                tv_1.setTextColor(getColor(R.color.cyan));
+                tv_2.setTextColor(getColor(R.color.cyan));
+                tv_3.setTextColor(getColor(R.color.cyan));
+                bt_next.setBackgroundColor(getColor(R.color.cyan));
+                rl.setBackgroundColor(getColor(R.color.black));
+                break;
+            case "Style3":
+                rb_Q1WA1Chemistry.setTextColor(getColor(R.color.brown));
+                rb_Q1WA2Chemistry.setTextColor(getColor(R.color.brown));
+                rb_Q1WA3Chemistry.setTextColor(getColor(R.color.brown));
+                rb_Q1RAChemistry.setTextColor(getColor(R.color.brown));
+                tv_1.setTextColor(getColor(R.color.brown));
+                tv_2.setTextColor(getColor(R.color.brown));
+                tv_3.setTextColor(getColor(R.color.brown));
+                bt_next.setBackgroundColor(getColor(R.color.brown));
+                rl.setBackgroundColor(getColor(R.color.gold));
+                break;
+        }
     }
 
 
--- app/src/main/java/svu/org/homework/SettingsActivity.java
+++ app/src/main/java/svu/org/homework/SettingsActivity.java
@@ -5,14 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.Switch;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
     Switch sw_sound;
     SharedPreferences sp;
     boolean sound;
+    String style;
+    RadioButton rb_style1,rb_style2,rb_style3;
+    Button bt_apply;
+    TextView tv_settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,40 @@ public class SettingsActivity extends AppCompatActivity {
         sound = sp.getBoolean("Sound",true);
         sw_sound = findViewById(R.id.sw_sound);
         sw_sound.setChecked(sound);
+        rb_style1 = findViewById(R.id.rb_style1);
+        rb_style2 = findViewById(R.id.rb_style2);
+        rb_style3 = findViewById(R.id.rb_style3);
+        bt_apply = findViewById(R.id.bt_apply);
+        tv_settings = findViewById(R.id.tv_settings);
+        switch(sp.getString("Style","Style1")){
+            case "Style1":
+                rb_style1.setChecked(true);
+                sw_sound.setTextColor(getColor(R.color.gold));
+                rb_style1.setTextColor(getColor(R.color.gold));
+                rb_style2.setTextColor(getColor(R.color.gold));
+                rb_style3.setTextColor(getColor(R.color.gold));
+                bt_apply.setBackgroundColor(getColor(R.color.gold));
+                tv_settings.setTextColor(getColor(R.color.gold));
+                break;
+            case "Style2":
+                rb_style2.setChecked(true);
+                sw_sound.setTextColor(getColor(R.color.cyan));
+                rb_style1.setTextColor(getColor(R.color.cyan));
+                rb_style2.setTextColor(getColor(R.color.cyan));
+                rb_style3.setTextColor(getColor(R.color.cyan));
+                bt_apply.setBackgroundColor(getColor(R.color.cyan));
+                tv_settings.setTextColor(getColor(R.color.cyan));
+                break;
+            case "Style3":
+                rb_style3.setChecked(true);
+                sw_sound.setTextColor(getColor(R.color.brown));
+                rb_style1.setTextColor(getColor(R.color.brown));
+                rb_style2.setTextColor(getColor(R.color.brown));
+                rb_style3.setTextColor(getColor(R.color.brown));
+                bt_apply.setBackgroundColor(getColor(R.color.brown));
+                tv_settings.setTextColor(getColor(R.color.brown));
+                break;
+        }
         sw_sound.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -30,6 +70,20 @@ public class SettingsActivity extends AppCompatActivity {
                 editor.apply();
             }
         });
+
+        bt_apply.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if (rb_style2.isChecked())style="Style2";
+                else if (rb_style3.isChecked())style="Style3";
+                else style="Style1";
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString("Style",style);
+                editor.apply();
+                recreate();
+            }
+        });
+
     }
 
 }
